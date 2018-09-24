5d4f759fad183841461b0aeb8221d36cf45fb02d
Christina Truong <christina.truong0@gmail.com>
2018-09-24 16:17:58 -0500

    assignments

---
+++ todolist.py
@@ -0,0 +1,18 @@
+class Todo:
+    def __init__(self, task, priority):
+        self.task = [ ]
+        self.priority = [ ]
+
+
+task = input("Enter task: ")
+priority = int(input("Rate priority between 1-3: "))
+
+while True:
+
+    if (task == "q"):
+        exit(0)
+    if (priority == "q"):
+        exit(0)
+
+
+self.task.append()
